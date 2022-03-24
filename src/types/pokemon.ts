22db3e6af008b37970022e9296fa8702b7b160bc
Margarita <mmvicuna1@uc.cl>
2022-03-24 20:43:15 -0300

    fix(typescript): add types file

---
+++ src/types/pokemon.ts
@@ -0,0 +1,24 @@
+export type Form = {
+    name: string
+}
+
+export type Slots = {
+    type: Type
+}
+
+export type Type = {
+    name: string
+}
+
+export type PokemonDetail = {
+    id: number;
+    forms: Form[];
+    types: Slots[];
+}
+
+export type PokemonName = {
+    name: string
+}
+
+
+
